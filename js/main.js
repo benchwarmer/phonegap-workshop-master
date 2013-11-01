4d72eafe4137d34fdefaf26352d1bc69c59c8a1d
benchwarmer <dschevyvette@gmail.com>
2013-11-01 17:55:30 -0500

    Trying to fix handlebars

--- js/main.js
+++ js/main.js
@@ -8,9 +8,9 @@ var app = {
     },
 
     initialize: function() {
+        this.homeTpl = Handlebars.compile($('#home-tpl').html());
+        this.employeeLiTpl = Handlebars.compile($('#employee-li-tpl').html());
         var self = this;
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
         this.store = new MemoryStore(function() {
             self.renderHomeView();
         });
