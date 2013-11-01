26b63a390cff408bc7057f4e0ba591c34d4b97bc
benchwarmer <dschevyvette@gmail.com>
2013-11-01 04:43:10 -0500

    Setting up a single page application

--- js/main.js
+++ js/main.js
@@ -16,16 +16,27 @@ var app = {
     initialize: function() {
         var self = this;
         this.store = new MemoryStore(function() {
-            self.showAlert('Store Initialized', 'Info');
+            self.renderHomeView();
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 
     showAlert: function(message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, "Ok")
-        } else
-        alert(title ? (title + ": " + message) : message );
+        } else {
+            alert(title ? (title + ": " + message) : message );
+        }
+    },
+
+    renderHomeView: function() {
+        var html =
+                "<div class='header'><h1>Home</h1></div>" +
+                "<div class='search-view'>" +
+                "<input class='search-key'/>" +
+                "<ul class='employee-list'></ul>" +
+                "</div>";
+        $('body').html(html);
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };
