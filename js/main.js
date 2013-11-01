b29b3152d57d347922f1518d0b6d56ec6285a9db
benchwarmer <dschevyvette@gmail.com>
2013-11-01 04:28:55 -0500

    Alert testing

--- js/main.js
+++ js/main.js
@@ -14,8 +14,18 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
+        var self = this;
+        this.store = new MemoryStore(function() {
+            self.showAlert('Store Initialized', 'Info');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
+    showAlert: function(message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, "Ok")
+        } else
+        alert(title ? (title + ": " + message) : message );
     }
 
 };
