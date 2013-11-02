0051a8e08b5c4763b512f746c3929c70ca67df2c
benchwarmer <dschevyvette@gmail.com>
2013-11-01 19:57:10 -0500

    Synced with correct code from section 6

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 var app = {
 
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+
     initialize: function() {
         var self = this;
         this.store = new MemoryStore(function() {
-            $('body').html(new Homeview(self.store).render().el)
+            $('body').html(new HomeView(self.store).render().el);
         });
-    },
-
-    showAlert: function(message, title) {
-        if (navigator.notification) {
-            navigator.notification.alert(message, null, title, "Ok")
-        } else {
-            alert(title ? (title + ": " + message) : message );
-        }
-    },
+    }
 
 };
 
